ca7431ad06fa4602cfdb3f745f539385af553572
Dray-OP <holongnhat2000@gmail.com>
2021-12-15 17:57:43 +0700

    desktop trang chủ

--- js/scriptLayout.js
+++ js/scriptLayout.js
@@ -9,13 +9,13 @@ $(".video-regular").slick({
     infinite: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
     responsive: [
         {
         breakpoint: 1024,
         settings: {
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
             infinite: true,
             dots: true
         }
@@ -44,7 +44,9 @@ $(".typical-products-regular").slick({
     infinite: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/icon_back.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../images/icon_right.png'>",
     responsive: [
         {
         breakpoint: 1024,
